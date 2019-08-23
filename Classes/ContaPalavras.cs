a8df337c4c13938f857a59fe768a6753bdb78247
Henrique Costa <hfc2007@gmail.com>
2019-08-23 09:06:21 -0300

    Add project files.

---
+++ Classes/ContaPalavras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Desafio.Classes
+{
+    class ContaPalavras
+    {
+        public int Executar(string palavra)
+        {
+
+            if (palavra.Length > 100)
+                Console.WriteLine("O texto digitado não pode ser superior a 100 digitos");
+            else {
+                        string[] textoCompleto = palavra.Split(',');
+                        int contador = 0;
+
+                        foreach (var item in textoCompleto)
+                       {
+                                contador += item.Trim().Length;                  
+                       } 
+
+                        return contador;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Desafio.Classes;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                
+                //Henrique Ferreira Costa
+
+                string palavra;
+                Console.Write("Digite o texto: ");
+                 palavra = Console.ReadLine();
+
+                int numeroPalavras = new ContaPalavras().Executar(palavra);
+
+                if (numeroPalavras > 0)
+               {
+                     Console.WriteLine("Letras no Total: " + numeroPalavras.ToString());
+               }
+                Console.ReadKey();
+            }
+    }
+}
